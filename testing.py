7c46029f74703116e898ae3f7ff8140701c78724
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-26 16:27:44 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,64 +1,66 @@
 import Matrices;
 import pytest;
+
+p = Matrices
     
 def test_invertirLista_1():
-    assert Portafolio2B.invertirLista([5,8,45,96]) == [96, 45, 8, 5]
+    assert p.invertirLista([5,8,45,96]) == [96, 45, 8, 5]
     
 def test_invertirLista_2():
-    assert Portafolio2B.invertirLista([56, 85,8,45,96]) == [96, 45, 8, 85, 56]
+    assert p.invertirLista([56, 85,8,45,96]) == [96, 45, 8, 85, 56]
 
 def test_invertirLista_2():
-    assert isinstance(Portafolio2B.invertirLista([]), str) == isinstance("Error: La lista debe contener al menos 2 elementos", str)
+    assert isinstance(p.invertirLista([]), str) == isinstance("Error: La lista debe contener al menos 2 elementos", str)
 
 def test_invertirLista_2():
-    assert isinstance(Portafolio2B.invertirLista([2,5,7,"ABC"]), str) == isinstance("Error: La lista debe contener elementos tipo entero", str)
+    assert isinstance(p.invertirLista([2,5,7,"ABC"]), str) == isinstance("Error: La lista debe contener elementos tipo entero", str)
     
     
 #####################################################################################################
 
 def test_extremosLista_1():
-    assert Portafolio2B.extremosLista([18,5,8,45,96, 60]) == [5,96]
+    assert p.extremosLista([18,5,8,45,96, 60]) == [5,96]
 
 def test_extremosLista_2():
-    assert Portafolio2B.extremosLista([96, 96,96]) == [96]
+    assert p.extremosLista([96, 96,96]) == [96]
 
 def test_extremosLista_3():
-    assert isinstance(Portafolio2B.extremosLista([]), str) == isinstance("Error: La lista debe contener al menos 2 elementos", str)
+    assert isinstance(p.extremosLista([]), str) == isinstance("Error: La lista debe contener al menos 2 elementos", str)
 
 def test_extremosLista_4():
-    assert isinstance(Portafolio2B.extremosLista([2,5,7,"ABC"]), str) == isinstance("Error: La lista debe contener elementos tipo entero", str)
+    assert isinstance(p.extremosLista([2,5,7,"ABC"]), str) == isinstance("Error: La lista debe contener elementos tipo entero", str)
     
 #####################################################################################################
 
 def test_eliminarElementosLista_1():
-    assert Portafolio2B.eliminarElementosLista([254, 25, 8], [25]) == [254, 8]
+    assert p.eliminarElementosLista([254, 25, 8], [25]) == [254, 8]
 
 def test_eliminarElementosLista_2():
-    assert Portafolio2B.eliminarElementosLista([54, 25, 8], [25, 54]) == [8]
+    assert p.eliminarElementosLista([54, 25, 8], [25, 54]) == [8]
 
 def test_eliminarElementosLista_3():
-    assert Portafolio2B.eliminarElementosLista([54, 25, 8], [21, 24]) == [54,25,8]
+    assert p.eliminarElementosLista([54, 25, 8], [21, 24]) == [54,25,8]
 
 def test_eliminarElementosLista_4():
-    assert isinstance(Portafolio2B.eliminarElementosLista([], [2,4]), str) == isinstance("Error: La primera lista debe contener al menos 1 elemento", str) 
+    assert isinstance(p.eliminarElementosLista([], [2,4]), str) == isinstance("Error: La primera lista debe contener al menos 1 elemento", str) 
 
 def test_eliminarElementosLista_5():
-    assert isinstance(Portafolio2B.eliminarElementosLista([23,78,9], []), str) == isinstance("Error: La segunda lista debe contener al menos 1 elemento", str) 
+    assert isinstance(p.eliminarElementosLista([23,78,9], []), str) == isinstance("Error: La segunda lista debe contener al menos 1 elemento", str) 
 
 def test_eliminarElementosLista_6():
-    assert isinstance(Portafolio2B.extremosLista([2,5,7,"ABC"], [2,8]), str) == isinstance("Error: La primera lista debe elementos tipo entero", str) 
+    assert isinstance(p.extremosLista([2,5,7,"ABC"], [2,8]), str) == isinstance("Error: La primera lista debe elementos tipo entero", str) 
 
 def test_eliminarElementosLista_7():
-    assert isinstance(Portafolio2B.extremosLista([2,5,7], [2,"8"]), str) == isinstance("Error: La segunda lista debe elementos tipo entero", str) 
+    assert isinstance(p.extremosLista([2,5,7], [2,"8"]), str) == isinstance("Error: La segunda lista debe elementos tipo entero", str) 
 
 #####################################################################################################
 
 def test_nivelesLista_1():
-    assert Portafolio2B.nivelesLista([ [[[[]]]], 2, [] ] ) == [4 ,0, 1]
+    assert p.nivelesLista([ [[[[]]]], 2, [] ] ) == [4 ,0, 1]
 def test_nivelesLista_2():
-    assert Portafolio2B.nivelesLista([ [2], [], [[[[[[]]]]]] ]) == [1, 1, 6]
+    assert p.nivelesLista([ [2], [], [[[[[[]]]]]] ]) == [1, 1, 6]
 def test_nivelesLista_3():
-    assert isinstance(Portafolio2B.nivelesLista(25), str) == isinstance("Error: El parámetro de entrada debe ser una lista", str) 
+    assert isinstance(p.nivelesLista(25), str) == isinstance("Error: El parámetro de entrada debe ser una lista", str) 
     
 #####################################################################################################
 
@@ -67,10 +69,10 @@ v2 = [-26, 2, 75 , 19 , -18, 23]
 v3 = [6, 2, 10 , 50, 90] 
 
 def test_obtenerIndicesListas_1():
-    assert Portafolio2B.obtenerIndicesMatriz([v1, v2, v3]) == [[2,3,4,5], [0,1,3,4,5], [1]]
+    assert p.obtenerIndicesMatriz([v1, v2, v3]) == [[2,3,4,5], [0,1,3,4,5], [1]]
 
 def test_obtenerIndicesListas_2():
-    assert isinstance(Portafolio2B.obtenerIndicesMatriz(25), str) == isinstance("Error: El parámetro de entrada debe ser una lista", str)
+    assert isinstance(p.obtenerIndicesMatriz(25), str) == isinstance("Error: El parámetro de entrada debe ser una lista", str)
     
 #########################################################################    
     
